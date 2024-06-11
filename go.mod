1cc10d3391c4edd061198881dbfe47c057431753
Damon <ddiazny919@gmail.com>
2024-06-11 05:58:44 -0400

    init

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/NoCapCbas/health-checker
+
+go 1.22.1
+
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "log"
+  "os"
+  "health-checker/check"
+)
+
+func main() {
+
+}
