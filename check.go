68fcb2fe19b1f95685b539461cfba0062b35e3bd
Damon <ddiazny919@gmail.com>
2024-06-11 06:18:19 -0400

    init

---
+++ check.go
@@ -0,0 +1,26 @@
+package check 
+
+import (
+  "fmt"
+  "net/http"
+  "time"
+)
+
+func HealthCheck(url string) (string, error) {
+  client := &http.Client{
+    Timeout: 30 * time.Second, 
+  }
+
+  resp, err := client.Get(url)
+  if err != nil {
+    return "", fmt.Errorf("could not make GET request: %w", err)
+  }
+
+  defer resp.Body.Close()
+
+  if resp.StatusCode == http.StatusOK {
+    return "Healthy", nil
+  }
+
+  return fmt.Sprintf("Unhealthy (status code: %d)", resp.StatusCode), nil
+}
--- main.go
+++ main.go
@@ -5,9 +5,23 @@ import (
   "fmt"
   "log"
   "os"
-  "health-checker/check"
 )
 
 func main() {
+
+  url := flag.String("url", "", "The URL to check the health of")
+  flag.Parse()
+
+  if *url == "" {
+    fmt.Println("Usage: healthchecker -url <URL>")
+    os.Exit(1)
+  }
+
+  status, err := HealthCheck(*url)
+  if err != nil {
+    log.Fatalf("Error checking health: %v\n", err)
+  }
+
+  fmt.Printf("Health check result for %s: %s\n", *url, status)
 
 }
