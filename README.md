b9a6871b5545515719ef5603e6fca4ed324eddc4
Damon Diaz <ddiazny919@gmail.com>
2024-06-17 11:11:48 -0400

    update main.go cli for HealthCheckPort

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A simple command-line tool to check the health of a given URL.
 
 ## Usage
 
+### Health Check Site
 Run the tool with the following command:
 
 ```bash
@@ -14,4 +15,25 @@ Replace <URL> with the URL you want to check.
 Healthy Example:
 ```bash
 go run main.go -url https://www.example.com/
+```
+
+### Health Check Port
+Run the tool with the following command:
+
+```bash
+go run main.go -port <PORT> -host <HOST>
+```
+Replace <PORT> with the port you want to check.
+Replace <HOST> with the host you want to check.
+
+Healthy Example:
+```bash
+go run main.go -port 80 -host example.com
+```
+
+
+## Test Health Checker
+Navigate to inside of healthchecker directory and run:
+```
+go test -v
 ```
--- main.go
+++ main.go
@@ -5,25 +5,37 @@ import (
   "fmt"
   "log"
   "os"
+  "strconv"
 
   "github.com/NoCapCbas/health-checker/healthchecker"
 )
 
 func main() {
-
+  // CLI flags
   url := flag.String("url", "", "The URL to check the health of")
+  port := flag.Int("port", 0, "The port number to check the health of")
+  host := flag.String("host", "", "The host to check the health of")
   flag.Parse()
 
-  if *url == "" {
-    fmt.Println("Usage: healthchecker -url <URL>")
+  if *url == "" && (*port == 0 && *host == "") {
+    fmt.Println("Usage: healthchecker -url <URL> OR -port <PORT> -host <HOST>")
     os.Exit(1)
   }
+  
+  if *url != "" {
+    status, err := healthchecker.HealthCheckSite(*url)
+    if err != nil {
+      log.Fatalf("Error checking health: %v\n", err)
+    }
 
-  status, err := healthchecker.HealthCheck(*url)
-  if err != nil {
-    log.Fatalf("Error checking health: %v\n", err)
+    fmt.Printf("Site Health check result for %s: %s\n", *url, status)
+  } else if *port != 0 && *host != "" {
+    portStr := strconv.Itoa(*port)
+    status, err := healthchecker.HealthCheckPort(*host, portStr)
+    if err != nil {
+      log.Fatalf("Error checking port health: %v\n", err)
+    }
+    fmt.Printf("Port health check result for %s:%s: %s\n", *host, portStr, status)
   }
-
-  fmt.Printf("Health check result for %s: %s\n", *url, status)
 
 }
